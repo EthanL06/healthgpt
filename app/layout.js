397fdd14c1633932886eaed3dfafbec7a32afe72
EthanL06 <ethanlanting@gmail.com>
2023-07-31 01:43:29 -0500

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
+"use client";
+
 import "./globals.css";
 import { Manrope } from "next/font/google";
+
+import Navbar from "@/components/Navbar";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -10,17 +14,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html data-theme="dark" lang="en">
       <body
-        className={`${manrope.className} flex flex-col items-center min-h-screen`}
+        className={`${manrope.className} flex flex-col items-center min-h-screen `}
       >
-        <div className="flex flex-col items-center w-full sm:w-[30rem] pt-8 px-12 text-center">
+        <Navbar />
+
+        <div className="flex flex-col items-center w-full sm:w-[30rem] pt-8 px-12 text-center relative">
           <h1 className="font-bold text-5xl">
             Health<span className="text-brand">GPT</span>
           </h1>
           <div className="text-xl">An AI-Powered Patient Diagnosis Tool</div>
           <div className="divider"></div>
         </div>
+
         {children}
       </body>
     </html>
